525169ae7820ab266e8ba1280def547d8806556c
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-07-10 01:50:49 +0900

    botがメッセージを送れるようにした

--- src/bot.py
+++ src/bot.py
@@ -12,7 +12,7 @@ class MyClient(discord.Client):
         
         if not (self.discord_token and self.channel_id):
             raise ValueError("DISCORD_TOKEN または CHANNEL_ID が設定されていません")
-        
+                
         intents = discord.Intents.default()
         intents.message_content = True  # メッセージの内容を取得する権限を付与
         
@@ -20,36 +20,12 @@ class MyClient(discord.Client):
     
     async def on_ready(self):
         print(f'{self.user} としてDiscordに接続しました!')    
+        channel = self.get_channel(int(self.channel_id))
+        if channel:
+            await channel.send("Botが起動しました!")
+        else:
+            print(f"チャンネルID {self.channel_id} が見つかりません。設定を確認してください。")
         
     def run_bot(self):
         self.run(self.discord_token)
-
-load_dotenv()
-
-# def Bot_Init():
-#     DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-#     CHANNEL_ID = os.getenv("CHANNEL_ID")
-
-#     if not (DISCORD_TOKEN and CHANNEL_ID):
-#         print("DISCORD_TOKEN または CHANNEL_ID が設定されていません")
-#         return
-
-#     intents = discord.Intents.default()
-#     intents.message_content = True  # メッセージの内容を取得する権限を付与
-
-#     client = MyClient(intents=intents)
-#     client.run(DISCORD_TOKEN)
-
-# async def Bot_Notify(Class: str):
-#     """
-#     discordのチャンネルに課題を追加するかどうかを確認するメッセージを送る
-#     Args:
-#         Class (str): 授業名
-#     Returns:
-#         None
-#     """
-    
-#     message = f"{Class}の課題を追加しますか?"
-    
-#     await client.send(message)
 
