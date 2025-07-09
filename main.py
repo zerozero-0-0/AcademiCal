3996c253995fc0d9e4b44a108fccb02624a10f00
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-07-10 00:36:17 +0900

    feat : DBを追加

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 from src.utils.read_json import read_json
+from src.database import DB_Connect
 
 def main(): 
     load_dotenv()
@@ -34,5 +35,6 @@ def read_json_test():
         print("JSONファイルの読み込みに失敗しました")
 
 if __name__ == "__main__":
+    DB_Connect()
     read_json_test()
     main()
--- src/database.py
+++ src/database.py
@@ -13,11 +13,40 @@ def DB_Connect():
             title TEXT NOT NULL,
             description TEXT,
             due_date TEXT,
-            status TEXT CHECK(status IN ('pending', 'completed')) DEFAULT 'pending',
-        )            
+            status TEXT CHECK(status IN ('pending','completed')) DEFAULT 'pending')            
     ''')
     
     cursor.close()
     conn.close()
     
+def DB_Insert(title, description, due_data):
+    conn = sqlite3.connect('assignment.db')
     
+    cur = conn.cursor()
+    
+    cur.execute('INSERT INTO assignments (title, description, due_date) VALUES (?, ?, ?)', (title, description, due_data))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def DB_Update(id, title, description, due_date, status):
+    conn = sqlite3.connect('assignment.db')    
+    cur = conn.cursor()
+    
+    cur.execute('UPDATE assignments SET title = ?, description = ?, due_date = ?, status = ? WHERE id = ?', (title, description, due_date, status, id))    
+
+    conn.commit()
+    cur.close()
+    conn.close()
+    
+def DB_Delete(id):
+    conn = sqlite3.connect('assignment.db')
+    
+    cur = conn.cursor()
+    
+    cur.execute('DELETE FROM assignments WHERE id = ?', (id,))
+    
+    conn.commit()
+    cur.close()
+    conn.close()
