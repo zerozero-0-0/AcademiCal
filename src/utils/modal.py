34bc572ca6ed8b22e6085b34ad4124ed805a7a49
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-07-11 11:41:12 +0900

    feat: 説明を入力必須でないようにした

--- src/utils/modal.py
+++ src/utils/modal.py
@@ -2,32 +2,46 @@ import discord
 from discord.ext import commands
 import os
 import datetime
+from src.database import DB_Insert
 
 # 1. GUIを表示
 # 2. 入力を受け取る
 # 3. 入力内容をdbに適用
 
+current_time = datetime.datetime.now()
+
+time = current_time.strftime("%m/%d %H:%M")
+
 class Modal(discord.ui.Modal):
     task_title = discord.ui.TextInput(
         label="課題名を入力",
         style=discord.TextStyle.short,
-        placeholder="課題名"
+        placeholder="課題名",
+        required=True
     )
+    
     task_description = discord.ui.TextInput(
         label="課題の説明を入力してください",
-        style=discord.TextStyle.long
+        style=discord.TextStyle.long,
+        required=False
     )
     
     task_due_date = discord.ui.TextInput(
         label="締切日を入力",
         style=discord.TextStyle.short,
-        placeholder="MM/DD hh:mm",
+        placeholder=time,
+        required=True
     )
 
     def __init__(self) -> None:
         super().__init__(title="AcademiCal Modal")
         
     async def on_submit(self, interaction: discord.Interaction) -> None:
+        DB_Insert(
+            title=self.task_title.value,
+            description=self.task_description.value,
+            due_data=self.task_due_date.value
+        )
         await interaction.response.send_message(
             f"課題{self.task_title.value}を登録しました"
         )
