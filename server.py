e59bb62fb48394ee3d7f4a8d0342a8b41cb3d78b
zerozero-0-0 <seapara.zerozero.00@gmail.com>
2025-07-11 14:16:21 +0900

    feat: koyeb にデプロイする準備

---
+++ server.py
@@ -0,0 +1,16 @@
+from threading import Thread
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Server is running"}
+
+def start():
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+    
+def server_thread():
+    t = Thread(target=start)
+    t.start()
